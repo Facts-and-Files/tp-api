a8af38daab9eba12dae0ce6c6e3e143387983f3b
trenc <github@tommyschmucker.de>
2022-01-21 16:58:32 +0100

    feat: add HtrData model

---
+++ app/HtrData.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HtrData extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * Config over convention here to respect exsting table names.
+     *
+     * @var string
+     */
+    protected $table = 'htr_data';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * Config over convention here to respect exsting column names.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'ItemId';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The storage format of the model's date columns.
+     *
+     * @var string
+     */
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+}
