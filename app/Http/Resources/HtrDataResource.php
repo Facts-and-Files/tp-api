2db610beeb7e3ceb20d42a0f5c24b9833c1cac85
trenc <github@tommyschmucker.de>
2022-02-02 13:40:56 +0100

    feat: add conventional resource

--- app/Http/Resources/HtrDataResource.php
+++ app/Http/Resources/HtrDataResource.php
@@ -10,15 +10,10 @@ class HtrDataResource extends JsonResource
      * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return array
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
-        return [
-            'id' => $this->ItemId,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-        ];
+        return parent::toArray($request);
     }
 }
-
