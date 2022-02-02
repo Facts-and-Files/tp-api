17d9de10621364f9dd0f08beb303c8d96c9cf27f
trenc <github@tommyschmucker.de>
2022-02-02 17:13:51 +0100

    docs: first OpenAPI documentation

--- app/Http/Controllers/HtrDataController.php
+++ app/Http/Controllers/HtrDataController.php
@@ -6,6 +6,13 @@ use App\Http\Controllers\BaseController as BaseController;
 use App\Models\HtrData as HtrData;
 use App\Http\Resources\HtrDataResource as HtrDataResource;
 
+/**
+ * @OA\Get(
+ *     path="/api/htrdata",
+ *     description="Landing page",
+ *     @OA\Response(response="", description="Landing page")
+ * )
+ */
 class HtrDataController extends BaseController
 {
     public function index()
