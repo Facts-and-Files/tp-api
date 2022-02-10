463aab8b004770fd774e258c7afaa36375a3eca8
trenc <github@tommyschmucker.de>
2022-02-10 10:40:36 +0100

    refactor: be more clear on naming the controllers for inheritance

--- src/app/Http/Controllers/HtrDataController.php
+++ src/app/Http/Controllers/HtrDataController.php
@@ -2,23 +2,73 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\BaseController as BaseController;
-use App\Models\HtrData as HtrData;
-use App\Http\Resources\HtrDataResource as HtrDataResource;
+use App\Http\Controllers\ResponseController;
+use App\Models\HtrData;
+use App\Http\Resources\HtrDataResource;
 
 /**
  * @OA\Get(
  *     path="/api/htrdata",
- *     description="Landing page",
- *     @OA\Response(response="", description="Landing page")
+ *     description="get all HTR data entries",
+ *     @OA\Response(response="", description="JSON response with all entries")
  * )
  */
-class HtrDataController extends BaseController
+class HtrDataController extends ResponseController
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $allHtrData = HtrData::all();
         return $this->sendResponse(HtrDataResource::collection($allHtrData), 'HtrData fetched.');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\HtrData  $htrData
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HtrData $htrData)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\HtrData  $htrData
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, HtrData $htrData)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\HtrData  $htrData
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HtrData $htrData)
+    {
+        //
     }
 
 }
