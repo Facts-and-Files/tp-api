0c0092e1dd328b95373c5227ba066c6e8b2a18bd
trenc <github@tommyschmucker.de>
2022-04-28 11:23:04 +0200

    fix: exit SSH sessions

--- deploy.sh
+++ deploy.sh
@@ -12,28 +12,53 @@ echo "Deploying to $STAGE..."
 date
 echo
 
-lftp -e "mirror --exclude vendor --exclude .env --include .env.$STAGE -R src $UPLOAD_PATH; bye" --user $SSH_USER --password $SSH_PASS sftp://$SSH_HOST
+if [ $STAGE = 'local' ]; then
+  lftp -c "mirror --exclude vendor --exclude .env --include .env.$STAGE -R src $UPLOAD_PATH; bye" --user $SSH_USER --password $SSH_PASS sftp://$SSH_HOST
+fi
+
+if [ $STAGE = 'dev' ]; then
+  lftp -c "set sftp:connect-program \"ssh -a -x -i $SSH_KEY\"; connect sftp://$SSH_USER@$SSH_HOST:$SSH_PORT; mirror --exclude vendor --exclude .env --include .env.$STAGE -R src $UPLOAD_PATH; bye"
+fi
 
 OUT=$?
 
-if [ $OUT = 0 ]; then
-  echo 'Transfer successful, moving to target...'
-  if [ $STAGE = 'local' ]; then
-    ssh -t $SSH_USER@$SSH_HOST << EOF
-    cd "$UPLOAD_PATH"
-    mv ".env.$STAGE" .env
-    composer install --optimize-autoloader --no-dev
-    php artisan optimize:clear
-    mkdir -p $INSTALL_PATH
-    mv $INSTALL_PATH "$INSTALL_PATH.$NOW"
-    cd .. && mv "$UPLOAD_PATH" "$INSTALL_PATH"
-    echo "$SSH_PASS"| sudo -S chown -R www-data:www-data "$INSTALL_PATH"
-EOF
-  fi
-  echo 'Deploy successful'
-else
+if [ $OUT != 0 ]; then
   echo 'Deploy failed'
+  exit 1
 fi
+
+
+echo 'Transfer successful, moving to target...'
+
+if [ $STAGE = 'local' ]; then
+  ssh -t $SSH_USER@$SSH_HOST << EOF
+  cd "$UPLOAD_PATH"
+  mv ".env.$STAGE" .env
+  composer install --optimize-autoloader --no-dev
+  php artisan optimize:clear
+  mkdir -p $INSTALL_PATH
+  mv $INSTALL_PATH "$INSTALL_PATH.$NOW"
+  cd .. && mv "$UPLOAD_PATH" "$INSTALL_PATH"
+  echo "$SSH_PASS"| sudo -S chown -R www-data:www-data "$INSTALL_PATH"
+  exit
+EOF
+fi
+
+if [ $STAGE = 'dev' ]; then
+  ssh -i $SSH_KEY -t $SSH_USER@$SSH_HOST -p $SSH_PORT << EOF
+  cd "$UPLOAD_PATH"
+  mv ".env.$STAGE" .env
+  composer install --optimize-autoloader --no-dev
+  php artisan optimize:clear
+  mkdir -p $INSTALL_PATH
+  mv $INSTALL_PATH "$INSTALL_PATH.$NOW"
+  cd .. && mv "$UPLOAD_PATH" "$INSTALL_PATH"
+  exit
+EOF
+fi
+
+echo 'Deploy successful'
 
 date
 echo
+
