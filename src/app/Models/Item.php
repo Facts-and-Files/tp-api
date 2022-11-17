5b4e5412511270c95e01ca8536a787f280697557
trenc <github@tommyschmucker.de>
2022-11-17 11:24:38 +0100

    feat: add Model for Items (restricted data access by now)

--- src/app/Models/Item.php
+++ src/app/Models/Item.php
@@ -22,16 +22,115 @@ class Item extends Model
     protected $table = 'Item';
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var string
-     */
-    protected $fillable = ['transcription_source'];
-
-    /**
      * The primary key associated with the table.
      *
      * @var string
      */
     protected $primaryKey = 'ItemId';
+
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array
+     */
+    /* protected $fillable = ['order_index', 'transcription_source']; */
+    protected $fillable = ['transcription_source'];
+
+    /**
+     * append new field names
+     *
+     * @var array
+     */
+    protected $appends = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * hide old field names
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'ItemId',
+        'Title',
+        'CompletionStatusId',
+        'StoryId',
+        'ProjectItemId',
+        'Description',
+        'DateStart',
+        'DateEnd',
+        'DatasetId',
+        'Timestamp',
+        'ImageLink',
+        'OrderIndex',
+        'TranscriptionStatusId',
+        'DescriptionStatusId',
+        'LocationStatusId',
+        'TaggingStatusId',
+        'AutomaticEnrichmentStatusId',
+        'Manifest',
+        'DescriptionLanguage',
+        'LockedTime',
+        'LockedUser',
+        'DateStartDisplay',
+        'DateEndDisplay',
+        'Exported',
+        'OldItemId',
+        'edm:WebResource',
+        'LastUpdated',
+        'EuropeanaAttachment',
+    ];
+
+    /**
+     * new id getter alias
+     *
+     * @return string
+     */
+    public function getIdAttribute()
+    {
+        return $this->attributes['ItemId'];
+    }
+
+    /**
+     * new created_at getter alias
+     *
+     * @return string
+     */
+    public function getCreatedAtAttribute()
+    {
+        return $this->attributes['Timestamp'];
+    }
+
+    /**
+     * new updated_at getter alias
+     *
+     * @return string
+     */
+    public function getUpdatedAtAttribute()
+    {
+        return $this->attributes['LastUpdated'];
+    }
+
+    /**
+     * new order_index getter alias
+     *
+     * @return string
+     */
+    /* public function getOrderIndexAttribute() */
+    /* { */
+    /*     return $this->attributes['OrderIndex']; */
+    /* } */
+
+    /**
+     * new order_index setter alias
+     *
+     * @return string
+     */
+    /* public function setOrderIndexAttribute($value) */
+    /* { */
+    /*     $this->attributes['OrderIndex'] = $value; */
+    /*     return $this->attributes['OrderIndex']; */
+    /* } */
 }
