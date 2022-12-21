e89ae9cffb9b0e97d4438841bfe1b403c98717fc
trenc <github@tommyschmucker.de>
2022-12-21 17:09:34 +0100

    feat: add controller for minimal GET endpoints

---
+++ src/app/Http/Controllers/StoryController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\ResponseController;
+use App\Models\Story;
+use App\Http\Resources\StoryResource;
+use Illuminate\Http\Request;
+
+class StoryController extends ResponseController
+{
+    /**
+     * Display a paginated listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $data = $this->getDataByRequest($request);
+
+        if (!$data) {
+            return $this->sendError('Invalid data', $request . ' not valid', 400);
+        }
+
+        $collection = StoryResource::collection($data);
+
+        return $this->sendResponse($collection, 'Stories fetched.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try {
+            $data = Story::findOrFail($id);
+            $resource = new StoryResource($data);
+
+            return $this->sendResponse($resource, 'Story fetched.');
+        } catch (\Exception $exception) {
+            return $this->sendError('Not found', $exception->getMessage());
+        }
+    }
+
+    /**
+     * Get data defined by request
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array \Illuminate\Database\Eloquent\Collection $data
+     */
+    protected function getDataByRequest(Request $request)
+    {
+        $queries = $request->query();
+
+        $queryColumns = [];
+
+        $story = new Story();
+
+        $data = $story->whereRaw('1 = 1');
+
+        foreach ($queries as $queryName => $queryValue) {
+            if (array_key_exists($queryName, $queryColumns)) {
+                $data->where($queryColumns[$queryName], $queryValue);
+            }
+        }
+
+        $data = $this->filterDataByQueries($data, $queries);
+
+        return $data;
+    }
+
+    /**
+     * Filter data by requested queries
+     *
+     * @param  \Illuminate\Http\Resources $data
+     * @param  array                      $queries
+     * @return array \Illuminate\Database\Eloquent\Collection $data
+     */
+    protected function filterDataByQueries($data, $queries)
+    {
+        $limit = $queries['limit'] ?? 100;
+        $page = $queries['page'] ?? 1;
+        $orderBy = $queries['orderBy'] ?? 'StoryId';
+        $orderBy = $orderBy === 'id' ? 'StoryId' : $orderBy;
+        $orderDir = $queries['orderDir'] ?? 'asc';
+        $offset = $limit * ($page - 1);
+
+        $filtered = $data
+            ->limit($limit)
+            ->offset($offset)
+            ->orderBy($orderBy, $orderDir)
+            ->get();
+
+        return $filtered;
+    }
+}
