62033ae4c70c33e99df2d60ddba9f10779cb776c
trenc <github@tommyschmucker.de>
2023-02-07 12:26:01 +0100

    feat: rearrange data to arrays for readability and simple consuming

--- src/app/Models/Story.php
+++ src/app/Models/Story.php
@@ -99,35 +99,12 @@ class Story extends Model
      */
     protected $appends = [
         'ItemIds',
-        'DcTitle',
-        'DcDescription',
-        'DcCreator',
-        'DcSource',
-        'DcContributor',
-        'DcPublisher',
-        'DcCoverage',
-        'DcDate',
-        'DcType',
-        'DcRelation',
-        'DcRights',
-        'DcLanguage',
-        'DcIdentifier',
-        'DctermsMedium',
-        'DctermsCreated',
-        'DctermsProvenance',
-        'EdmLandingPage',
-        'EdmCountry',
-        'EdmDataProvider',
-        'EdmProvider',
-        'EdmRights',
-        'EdmYear',
-        'EdmDatasetName',
-        'EdmBegin',
-        'EdmEnd',
-        'EdmIsShownAt',
-        'EdmLanguage',
-        'EdmAgent'
+        'Dcterms',
+        'Dc',
+        'Edm',
+        'Place'
     ];
+
 
     /**
      * Get the ItemIds for the story.
@@ -138,226 +115,66 @@ class Story extends Model
     }
 
     /**
-     * Get the DcTitle for the story.
+     * Get the dcterm object
      */
-    public function getDcTitleAttribute()
-    {
-        return $this->attributes['dc:title'];
+    public function getDctermsAttribute() {
+        return [
+            'Medium'     => $this->attributes['dcterms:medium'],
+            'Created'    => $this->attributes['dcterms:created'],
+            'Provenance' => $this->attributes['dcterms:provenance'],
+        ];
     }
 
     /**
-     * Get the DcDescription for the story.
+     * Get the dc object
      */
-    public function getDcDescriptionAttribute()
-    {
-        return $this->attributes['dc:description'];
+    public function getDcAttribute() {
+        return [
+            'Title'       => $this->attributes['dc:title'],
+            'Description' => $this->attributes['dc:description'],
+            'Creator'     => $this->attributes['dc:creator'],
+            'Source'      => $this->attributes['dc:source'],
+            'Contributor' => $this->attributes['dc:contributor'],
+            'Publisher'   => $this->attributes['dc:publisher'],
+            'Coverage'    => $this->attributes['dc:coverage'],
+            'Date'        => $this->attributes['dc:date'],
+            'Type'        => $this->attributes['dc:type'],
+            'Relation'    => $this->attributes['dc:relation'],
+            'Rights'      => $this->attributes['dc:rights'],
+            'Language'    => $this->attributes['dc:language'],
+            'Identifier'  => $this->attributes['dc:identifier']
+        ];
     }
 
     /**
-     * Get the DcCreator for the story.
+     * Get the edm object for the story.
      */
-    public function getDcCreatorAttribute()
-    {
-        return $this->attributes['dc:creator'];
+    public function getEdmAttribute() {
+        return [
+            'LandingPage'  => $this->attributes['edm:landingPage'],
+            'Country'      => $this->attributes['edm:country'],
+            'DataProvider' => $this->attributes['edm:dataProvider'],
+            'Provider'     => $this->attributes['edm:provider'],
+            'Rights'       => $this->attributes['edm:rights'],
+            'Year'         => $this->attributes['edm:year'],
+            'DatasetName'  => $this->attributes['edm:datasetName'],
+            'Begin'        => $this->attributes['edm:begin'],
+            'End'          => $this->attributes['edm:end'],
+            'IsShownAt'    => $this->attributes['edm:isShownAt'],
+            'Language'     => $this->attributes['edm:language'],
+            'Agent'        => $this->attributes['edm:agent']
+        ];
     }
-
     /**
-     * Get the DcSource for the story.
-     */
-    public function getDcSourceAttribute()
-    {
-        return $this->attributes['dc:source'];
-    }
 
-    /**
-     * Get the DcContributor for the story.
+     * Get the place object of the story
      */
-    public function getDcContributorAttribute()
-    {
-        return $this->attributes['dc:contributor'];
-    }
-
-    /**
-     * Get the DcPublisher for the story.
-     */
-    public function getDcPublisherAttribute()
-    {
-        return $this->attributes['dc:publisher'];
-    }
-
-    /**
-     * Get the DcCoverage for the story.
-     */
-    public function getDcCoverageAttribute()
-    {
-        return $this->attributes['dc:coverage'];
-    }
-
-    /**
-     * Get the dc:date for the story.
-     */
-    public function getDcDateAttribute()
-    {
-        return $this->attributes['dc:date'];
-    }
-
-    /**
-     * Get the dc:type for the story.
-     */
-    public function getDcTypeAttribute()
-    {
-        return $this->attributes['dc:type'];
-    }
-
-    /**
-     * Get the dc:relation for the story.
-     */
-    public function getDcRelationAttribute()
-    {
-        return $this->attributes['dc:relation'];
-    }
-
-    /**
-     * Get the dc:rights for the story.
-     */
-    public function getDcRightsAttribute()
-    {
-        return $this->attributes['dc:rights'];
-    }
-
-    /**
-     * Get the dc:language for the story.
-     */
-    public function getDcLanguageAttribute()
-    {
-        return $this->attributes['dc:language'];
-    }
-
-    /**
-     * Get the dc:identifier for the story.
-     */
-    public function getDcIdentifierAttribute()
-    {
-        return $this->attributes['dc:identifier'];
-    }
-
-    /**
-     * Get the dcterms:medium for the story.
-     */
-    public function getDctermsMediumAttribute()
-    {
-        return $this->attributes['dcterms:medium'];
-    }
-
-    /**
-     * Get the dcterms:created for the story.
-     */
-    public function getDctermsCreatedAttribute()
-    {
-        return $this->attributes['dcterms:created'];
-    }
-
-    /**
-     * Get the dcterms:provenance for the story.
-     */
-    public function getDctermsProvenanceAttribute()
-    {
-        return $this->attributes['dcterms:provenance'];
-    }
-
-    /**
-     * Get the edm:landingPage for the story.
-     */
-    public function getEdmLandingPageAttribute()
-    {
-        return $this->attributes['edm:landingPage'];
-    }
-
-    /**
-     * Get the edm:country for the story.
-     */
-    public function getEdmCountryAttribute()
-    {
-        return $this->attributes['edm:country'];
-    }
-
-    /**
-     * Get the edm:dataProvider for the story.
-     */
-    public function getEdmDataProviderAttribute()
-    {
-        return $this->attributes['edm:dataProvider'];
-    }
-
-    /**
-     * Get the edm:provider for the story.
-     */
-    public function getEdmProviderAttribute()
-    {
-        return $this->attributes['edm:provider'];
-    }
-
-    /**
-     * Get the edm:rights for the story.
-     */
-    public function getEdmRightsAttribute()
-    {
-        return $this->attributes['edm:rights'];
-    }
-
-    /**
-     * Get the edm:year for the story.
-     */
-    public function getEdmYearAttribute()
-    {
-        return $this->attributes['edm:year'];
-    }
-
-    /**
-     * Get the edm:datasetName for the story.
-     */
-    public function getEdmDatasetNameAttribute()
-    {
-        return $this->attributes['edm:datasetName'];
-    }
-
-    /**
-     * Get the edm:begin for the story.
-     */
-    public function getEdmBeginAttribute()
-    {
-        return $this->attributes['edm:begin'];
-    }
-
-    /**
-     * Get the edm:end for the story.
-     */
-    public function getEdmEndAttribute()
-    {
-        return $this->attributes['edm:end'];
-    }
-
-    /**
-     * Get the edm:isShownAt for the story.
-     */
-    public function getEdmIsShownAtAttribute()
-    {
-        return $this->attributes['edm:isShownAt'];
-    }
-
-    /**
-     * Get the edm:language for the story.
-     */
-    public function getEdmLanguageAttribute()
-    {
-        return $this->attributes['edm:language'];
-    }
-
-    /**
-     * Get the edm:agent for the story.
-     */
-    public function getEdmAgentAttribute()
-    {
-        return $this->attributes['edm:agent'];
+    public function getPlaceAttribute() {
+        return [
+            'Name'      => $this->attributes['PlaceName'],
+            'Latitude'  => $this->attributes['PlaceLatitude'],
+            'Longitude' => $this->attributes['PlaceLongitude'],
+            'Zoom'      => $this->attributes['placeZoom']
+        ];
     }
 }
