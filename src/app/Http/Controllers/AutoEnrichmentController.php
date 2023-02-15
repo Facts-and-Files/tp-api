a1e0df95b9b395aa4e84eaa687ad25444beca7a1
trenc <github@tommyschmucker.de>
2023-02-15 14:56:22 +0100

    docs: remove unused web form view

--- src/app/Http/Controllers/AutoEnrichmentController.php
+++ src/app/Http/Controllers/AutoEnrichmentController.php
@@ -28,16 +28,6 @@ class AutoEnrichmentController extends ResponseController
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
