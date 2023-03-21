0b3bef0ae2c213907dcf7bf00703d2bb57e2accd
Vejn <jsalesev@gmail.com>
2023-03-21 10:03:00 +0100

    new models

---
+++ src/app/Models/Property.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Property extends Model
+{
+    /**
+     * Use user defined exisiting timestamp columns
+     */
+    const CREATED_AT = null;
+    const UPDATED_AT = null;
+
+    /**
+     * The table associated with the model.
+     *
+     * Config over convention here to respect exsting table names.
+     *
+     * @var string
+     */
+    protected $table = 'Property';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'PropertyId';
+}
