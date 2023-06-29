af9ff55067c0569fdfd983199c09b64bd7ce7505
trenc <github@tommyschmucker.de>
2023-06-29 12:39:55 +0200

    test: provide basic response test for authorization

---
+++ src/tests/Feature/ResponseTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Support\Str;
+
+class ResponseTest extends TestCase
+{
+    public function testNoTokenCallsOnProjectsShouldResponse_401(): void
+    {
+        $response = $this->withoutToken()->get('/projects');
+
+        $response->assertStatus(401);
+    }
+
+    public function testWrongTokenCallsOnProjectsShouldResponse_401(): void
+    {
+        $randomKey = Str::random($length = 50);
+
+        $response = $this->withToken($randomKey)->get('/projects');
+
+        $response->assertStatus(401);
+    }
+
+    public function testAuthorizedCallsOnProjectsShouldNotResponse_401(): void
+    {
+        $response = $this->get('/projects');
+        $statusCode = $response->getStatusCode();
+
+        $this->assertNotEquals($statusCode, 401);
+    }
+
+    public function testWrongUriCallOnProjectShouldResponse_404(): void
+    {
+        $response = $this->get('/project');
+
+        $response->assertStatus(404);
+    }
+}
