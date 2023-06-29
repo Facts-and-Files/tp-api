d5a7ae9eceeac197c28fbc3abca0b72bf026e392
trenc <github@tommyschmucker.de>
2023-06-29 12:39:05 +0200

    test: populate test db and prepare test cases with defaults

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -3,8 +3,32 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withToken('8je2CZ8r1U1JUdclpfSVKyP6gSzF65c16Q6bY6P2EqpGAWSwLvgHjhfuu4FS');
+
+        $this->makeMigrations();
+    }
+
+    protected function makeMigrations(): void
+    {
+        $files = glob('storage/test-migrations/*.sqlite');
+
+        foreach ($files as $file) {
+
+            $sql = File::get($file);
+
+            DB::unprepared($sql);
+
+        }
+    }
 }
