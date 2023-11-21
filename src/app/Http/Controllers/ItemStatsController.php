3493dbea76e2334638523b21d81bbc7a1583d912
trenc <github@tommyschmucker.de>
2023-11-21 16:22:19 +0100

    feat: create first responses for itemStats

--- src/app/Http/Controllers/ItemStatsController.php
+++ src/app/Http/Controllers/ItemStatsController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\ResponseController;
+use App\Models\Item;
 use App\Models\ItemStats;
+use App\Models\Transcription;
 use App\Http\Resources\ItemStatsResource;
 
 class ItemStatsController extends ResponseController
@@ -41,5 +43,95 @@ class ItemStatsController extends ResponseController
         } catch (\Exception $exception) {
             return $this->sendError('Not found', $exception->getMessage());
         }
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $data = [];
+
+        try {
+            foreach ($request['ItemIds'] as $itemId) {
+                $item = Item::findOrfail($itemId);
+
+                // transcription chars count
+                $transcriptionCount = isset($item->Transcription->TranscriptionText)
+                    ? mb_strlen($item->Transcription->TranscriptionText, 'UTF-8')
+                    : 0;
+
+                // create or update
+                $itemStats = ItemStats::find($itemId) ?: new ItemStats();
+
+                // populate other counts and stats
+                $itemStats->ItemId = $item->ItemId;
+                $itemStats->StoryId = $item->StoryId;
+                $itemStats->Enrichments = $this->getEnrichmentsCounts($item);
+                $itemStats->TranscribedCharsManual = $item->TranscriptionSource === 'manual' ? $transcriptionCount : 0;
+                $itemStats->TranscribedCharsHtr = $item->TranscriptionSource === 'htr' ? $transcriptionCount : 0;
+                $itemStats->EditStart = $this->getOldestTranscriptionDate($item);
+                $itemStats->UserIds = [];
+
+                $itemStats->save();
+
+                $data[] = $itemStats;
+            }
+
+            $resource = new ItemStatsResource($data);
+
+            return $this->sendResponse($resource, 'HtrData inserted.');
+        } catch (\Exception $exception) {
+            return $this->sendError('Invalid data', $exception->getMessage(), 400);
+        }
+    }
+
+    protected function getEnrichmentsCounts(Item $item): array
+    {
+        $enrichments = [];
+        $enrichments['Description'] = !empty($item->Description) ? 1 : 0;
+        $enrichments['Places'] = $item->Places->count();
+        $enrichments['Persons'] = $item->Persons->count();
+        $enrichments['Properties'] = $item->Properties->count();
+        $enrichments['Dates'] = $this->countDates($item);
+
+        return $enrichments;
+    }
+
+    protected function getOldestTranscriptionDate(Item $item): string|null
+    {
+        // manual transcription begin date
+        $editStartManual = $item
+            ->hasMany(Transcription::class, 'ItemId')
+            ->orderBy('Timestamp', 'asc')
+            ->first('Timestamp');
+
+        // htr transcription begin date
+        $editStartHtr = $item
+            ->htrData()
+            ->with(['htrDataRevision' => function ($query) {
+                $query
+                    ->orderBy('Timestamp', 'asc')
+                    ->first();
+            }])
+            ->orderBy('Timestamp', 'asc')
+            ->first('Timestamp');
+
+        $editStartHtrDatetime = $editStartHtr
+            ? date('Y-m-d H:i:s', strtotime($editStartHtr->Timestamp))
+            : null;
+
+        $editStartManualDatetime = $editStartManual
+            ? date('Y-m-d H:i:s', strtotime($editStartManual->Timestamp))
+            : null;
+
+        return $editStartManualDatetime ?: $editStartHtrDatetime ?: null;
+    }
+
+    protected function countDates(Item $item): int
+    {
+        $count = 0;
+
+        $count += !empty($item->DateStartDisplay) ? 1 : 0;
+        $count += !empty($item->DateEndDisplay) ? 1 : 0;
+
+        return $count;
     }
 }
