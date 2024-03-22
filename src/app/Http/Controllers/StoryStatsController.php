6c0c2896503a643c94758d1b39d9c795707309aa
trenc <github@tommyschmucker.de>
2024-03-22 13:07:27 +0100

    refactor: remove obsolete import

--- src/app/Http/Controllers/StoryStatsController.php
+++ src/app/Http/Controllers/StoryStatsController.php
@@ -8,7 +8,6 @@ use App\Models\Item;
 use App\Models\ItemStats;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\DB;
 
 class StoryStatsController extends ResponseController
 {
