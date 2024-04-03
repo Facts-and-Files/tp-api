289c63d673f4c79b90efe3fea8c2199bf8b0f2c9
trenc <github@tommyschmucker.de>
2024-04-03 10:11:39 +0200

    refactor: remove obsolete websocket trait

--- src/app/Events/ScoreTableUpdated.php
+++ src/app/Events/ScoreTableUpdated.php
@@ -3,13 +3,12 @@
 namespace App\Events;
 
 use App\Models\Score;
-use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class ScoreTableUpdated
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, SerializesModels;
 
     public $score;
 
