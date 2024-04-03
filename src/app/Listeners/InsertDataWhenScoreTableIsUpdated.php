d11834083eb1926248d3c11c76c866f902ca7fda
trenc <github@tommyschmucker.de>
2024-04-03 11:06:57 +0200

    feat: add listener to update TeamScore

--- src/app/Listeners/InsertDataWhenScoreTableIsUpdated.php
+++ src/app/Listeners/InsertDataWhenScoreTableIsUpdated.php
@@ -3,11 +3,18 @@
 namespace App\Listeners;
 
 use App\Events\ScoreTableUpdated;
+use App\Models\User;
 
 class InsertDataWhenScoreTableIsUpdated
 {
     public function handle(ScoreTableUpdated $event): void
     {
-        print_r($event->score);
+        $score = $event->score;
+        $user = User::find($score->UserId);
+
+        if ($user) {
+            $teamIds = $user->teams->pluck('TeamId')->toArray();
+            $score->teams()->sync($teamIds);
+        }
     }
 }
