47977f83a290d79948744293801d507fd552e97c
trenc <github@tommyschmucker.de>
2024-04-04 15:41:27 +0200

    feat: add teamStatistics to campaignStats endpoint

--- src/app/Http/Controllers/CampaignStatsController.php
+++ src/app/Http/Controllers/CampaignStatsController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\ResponseController;
 use App\Http\Resources\CampaignStatsResource;
+use App\Models\Campaign;
+use App\Models\Score;
+use App\Models\ScoreType;
+use App\Traits\Rename;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
 class CampaignStatsController extends ResponseController
 {
+    use Rename;
+
     public function show(int $id): JsonResponse
     {
         try {
@@ -31,9 +37,12 @@ class CampaignStatsController extends ResponseController
                 $users[] = $user;
             });
 
+            $teams = $this->buildTeamStatistics($id);
+
             $grouped = [
                 'CampaignId' => $id,
                 'Summary'    => $summary,
+                'Teams'      => $teams,
                 'Users'      => $users
             ];
 
@@ -43,6 +52,67 @@ class CampaignStatsController extends ResponseController
         } catch (\Exception $exception) {
             return $this->sendError('Invalid data', $exception->getMessage(), 400);
         }
+    }
+
+    protected function buildTeamStatistics(int $campaignId): array
+    {
+        $campaign = Campaign::find($campaignId);
+
+        $campaignItemIds = $campaign->stories->pluck('ItemIds')->flatten()->all();
+
+        $scoreTypes = ScoreType::get();
+
+        $allTeamsStats = [];
+
+        foreach ($campaign->teams as $team) {
+            $teamStats = [
+                'TeamId' => $team['TeamId'],
+                'Miles' => 0
+            ];
+
+            $teamScoreIds = DB::table('TeamScore')
+                ->where('TeamId', $team['TeamId'])
+                ->pluck('ScoreId')
+                ->all();
+
+            $teamCampaignScores = Score::whereIn('ScoreId', $teamScoreIds)
+                ->whereIn('ItemId', $campaignItemIds)
+                ->whereBetween('Timestamp', [$campaign->Start, $campaign->End])
+                ->get();
+
+            $scoreTypes->map(function ($score) use (&$teamStats) {
+                $scoreName = $this->rename($score->Name);
+                $teamStats[$scoreName] = 0;
+            });
+
+            $teamCampaignScores->map(function ($score) use ($scoreTypes, &$teamStats) {
+                $nameAndRates = $scoreTypes->where('ScoreTypeId', $score->ScoreTypeId);
+                $name = $this->rename($nameAndRates->pluck('Name')->first());
+                $rate = $nameAndRates->pluck('Rate')->first();
+                $teamStats[$name] += $score->Amount;
+                $teamStats['Miles'] += $score->Amount * $rate;
+            });
+
+
+            $teamStats['Stories'] = $campaign
+                ->stories
+                ->filter(function ($story) use ($teamCampaignScores) {
+                    $itemIds = array_merge(
+                        $story->ItemIds->all(),
+                        $teamCampaignScores->pluck('ItemId')->all()
+                    );
+                    return count(array_unique($itemIds)) > 0 ? true : false;
+                })
+                ->pluck('StoryId')
+                ->unique()
+                ->count();
+            $teamStats['Items'] = $teamCampaignScores->pluck('ItemId')->unique()->count();
+            $teamStats['Miles'] = ceil($teamStats['Miles']);
+
+            $allTeamsStats[] = $teamStats;
+        }
+
+        return $allTeamsStats;
     }
 
     protected function buildCampaignStatisticsSummary(Collection $collection): array
