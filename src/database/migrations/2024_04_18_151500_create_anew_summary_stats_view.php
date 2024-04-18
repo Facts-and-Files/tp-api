04fa0ec2f9ec7d5f8930f4f8ba280ff90f16e2fd
trenc <github@tommyschmucker.de>
2024-04-18 12:26:32 +0200

    feat: new migration for summary_stats_view with itemsStarted column

---
+++ src/database/migrations/2024_04_18_151500_create_anew_summary_stats_view.php
@@ -0,0 +1,89 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        DB::statement('DROP VIEW IF EXISTS summary_stats_view');
+
+        DB::statement('
+            CREATE VIEW summary_stats_view AS
+            SELECT
+                t1.Year,
+                t1.Month,
+                t1.ScoreTypeId,
+                t1.UniqueUsers AS UniqueUsersPerScoreType,
+                t1.UniqueItems AS UniqueItemsPerScoreType,
+                t2.UniqueUsers AS OverallUniqueUsers,
+                t2.UniqueItems AS OverallUniqueItems,
+                COALESCE(t3.NumberOfFirstRecords, 0) AS OverallItemsStarted,
+                t1.Amount
+            FROM
+                (
+                    SELECT
+                        YEAR(Timestamp) AS Year,
+                        MONTH(Timestamp) AS Month,
+                        ScoreTypeId,
+                        COUNT(DISTINCT UserId) AS UniqueUsers,
+                        COUNT(DISTINCT ItemId) AS UniqueItems,
+                        SUM(Amount) AS Amount
+                    FROM
+                        Score
+                    GROUP BY
+                        YEAR(Timestamp),
+                        MONTH(Timestamp),
+                        ScoreTypeId
+                ) AS t1
+            JOIN
+                (
+                    SELECT
+                        YEAR(Timestamp) AS Year,
+                        MONTH(Timestamp) AS Month,
+                        COUNT(DISTINCT UserId) AS UniqueUsers,
+                        COUNT(DISTINCT ItemId) AS UniqueItems
+                    FROM
+                        Score
+                    GROUP BY
+                        YEAR(Timestamp),
+                        MONTH(Timestamp)
+                ) AS t2
+        ON
+            t1.Year = t2.Year
+            AND t1.Month = t2.Month
+        LEFT JOIN
+            (
+                SELECT
+                    YEAR(s.Timestamp) AS Year,
+                    MONTH(s.Timestamp) AS Month,
+                    COUNT(*) AS NumberOfFirstRecords
+                FROM
+                    Score s
+                INNER JOIN (
+                    SELECT
+                        ItemId,
+                        MIN(ScoreId) AS FirstScoreId
+                    FROM
+                        Score
+                    GROUP BY
+                        ItemId
+                ) AS firstRecords
+                ON
+                    s.ItemId = firstRecords.ItemId
+                    AND s.ScoreId = firstRecords.FirstScoreId
+                GROUP BY
+                    YEAR(s.Timestamp), MONTH(s.Timestamp)
+            ) AS t3
+        ON
+            t1.Year = t3.Year
+            AND t1.Month = t3.Month;
+        ');
+    }
+
+    public function down(): void
+    {
+        DB::statement('DROP VIEW IF EXISTS summary_stats_view');
+    }
+};
