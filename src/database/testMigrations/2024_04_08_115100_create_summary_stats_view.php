e95cf488836048a2fc11947eaa12f1aeb401456b
trenc <github@tommyschmucker.de>
2024-04-18 12:45:06 +0200

    tests: enhance test for summary stats with items started

--- src/database/testMigrations/2024_04_08_115100_create_summary_stats_view.php
+++ src/database/testMigrations/2024_04_08_115100_create_summary_stats_view.php
@@ -10,23 +10,38 @@ return new class extends Migration
         DB::statement("
             CREATE VIEW summary_stats_view AS
             SELECT
-                strftime('%Y', Timestamp) AS Year,
-                strftime('%m', Timestamp) AS Month,
-                ScoreTypeId,
-                COUNT(DISTINCT UserId) AS UniqueUsersPerScoreType,
-                COUNT(DISTINCT ItemId) AS UniqueItemsPerScoreType,
+                t1.Year,
+                t1.Month,
+                t1.ScoreTypeId,
+                t1.UniqueUsers AS UniqueUsersPerScoreType,
+                t1.UniqueItems AS UniqueItemsPerScoreType,
                 t2.UniqueUsers AS OverallUniqueUsers,
                 t2.UniqueItems AS OverallUniqueItems,
-                SUM(Amount) AS Amount
+                t1.Amount,
+                COALESCE(t3.NumberOfFirstRecords, 0) AS OverallItemsStarted
             FROM
-                Score
+                (
+                    SELECT
+                        strftime('%Y', Timestamp) AS Year,
+                        strftime('%m', Timestamp) AS Month,
+                        ScoreTypeId,
+                        COUNT(DISTINCT UserId) AS UniqueUsers,
+                        COUNT(DISTINCT ItemId) AS UniqueItems,
+                        SUM(Amount) AS Amount
+                    FROM
+                        Score
+                    GROUP BY
+                        strftime('%Y', Timestamp),
+                        strftime('%m', Timestamp),
+                        ScoreTypeId
+                ) AS t1
             JOIN
                 (
                     SELECT
                         strftime('%Y', Timestamp) AS Year,
                         strftime('%m', Timestamp) AS Month,
                         COUNT(DISTINCT UserId) AS UniqueUsers,
-                        COUNT(DISTINCT UserId) AS UniqueItems
+                        COUNT(DISTINCT ItemId) AS UniqueItems
                     FROM
                         Score
                     GROUP BY
@@ -34,10 +49,34 @@ return new class extends Migration
                         strftime('%m', Timestamp)
                 ) AS t2
             ON
-                strftime('%Y', Score.Timestamp) = t2.Year
-                AND strftime('%m', Score.Timestamp) = t2.Month
-            GROUP BY
-                Year, Month, ScoreTypeId;
+                t1.Year = t2.Year
+                AND t1.Month = t2.Month
+            LEFT JOIN
+                (
+                    SELECT
+                        strftime('%Y', s.Timestamp) AS Year,
+                        strftime('%m', s.Timestamp) AS Month,
+                        COUNT(*) AS NumberOfFirstRecords
+                    FROM
+                        Score s
+                    INNER JOIN (
+                        SELECT
+                            ItemId,
+                            MIN(ScoreId) AS FirstScoreId
+                        FROM
+                            Score
+                        GROUP BY
+                            ItemId
+                    ) AS firstRecords
+                    ON
+                        s.ItemId = firstRecords.ItemId
+                        AND s.ScoreId = firstRecords.FirstScoreId
+                    GROUP BY
+                        strftime('%Y', s.Timestamp), strftime('%m', s.Timestamp)
+                ) AS t3
+            ON
+                t1.Year = t3.Year
+                AND t1.Month = t3.Month;
         ");
     }
 
