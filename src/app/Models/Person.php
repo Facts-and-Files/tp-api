4163ebc8db9e6cd8b9432d34a7da1ed4109a543a
trenc <github@tommyschmucker.de>
2024-06-12 10:01:57 +0200

    refactor: remove docblocks

--- src/app/Models/Person.php
+++ src/app/Models/Person.php
@@ -6,25 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    /**
-     * Use user defined exisiting timestamp columns
-     */
     const CREATED_AT = null;
     const UPDATED_AT = null;
 
-    /**
-     * The table associated with the model.
-     *
-     * Config over convention here to respect exsting table names.
-     *
-     * @var string
-     */
     protected $table = 'Person';
 
-    /**
-     * The primary key associated with the table.
-     *
-     * @var string
-     */
     protected $primaryKey = 'PersonId';
 }
