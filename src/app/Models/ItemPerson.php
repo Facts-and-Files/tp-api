4cb188f84612802ec1f73c0093ef42e134b2edab
trenc <github@tommyschmucker.de>
2024-06-12 11:21:43 +0200

    fix: use correct belongsToMany for manyToMany relationship for Person

---
+++ src/app/Models/ItemPerson.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ItemPerson extends Model
+{
+    const CREATED_AT = null;
+    const UPDATED_AT = null;
+
+    protected $table = 'ItemPerson';
+
+    protected $primaryKey = 'ItemPersonId';
+}
--- src/app/Models/Person.php
+++ src/app/Models/Person.php
@@ -12,4 +12,6 @@ class Person extends Model
     protected $table = 'Person';
 
     protected $primaryKey = 'PersonId';
+
+    protected $hidden = ['pivot'];
 }
