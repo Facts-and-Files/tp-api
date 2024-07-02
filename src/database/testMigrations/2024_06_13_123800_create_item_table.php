ba9f945823e37a8d21168dad1c8e15bf0ecd47f4
trenc <github@tommyschmucker.de>
2024-07-02 11:41:35 +0200

    tests: update test migration with TaggingStatusId column

--- src/database/testMigrations/2024_06_13_123800_create_item_table.php
+++ src/database/testMigrations/2024_06_13_123800_create_item_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
 
             $table->increments('ItemId');
             $table->smallInteger('CompletionStatusId');
+            $table->smallInteger('TranscriptionStatusId');
             $table->smallInteger('TaggingStatusId');
         });
     }
