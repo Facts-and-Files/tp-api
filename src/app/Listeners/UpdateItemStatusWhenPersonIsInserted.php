57e85ad3a601dc06c4c06dc75adb6ca244c861c1
trenc <github@tommyschmucker.de>
2024-07-02 11:41:07 +0200

    fix: update also TaggingStatusId when inserting a person

--- src/app/Listeners/UpdateItemStatusWhenPersonIsInserted.php
+++ src/app/Listeners/UpdateItemStatusWhenPersonIsInserted.php
@@ -9,18 +9,21 @@ class UpdateItemStatusWhenPersonIsInserted
 {
     public function handle(PersonInserted $event): void
     {
-        $person = $event->person;
-        $item = Item::find($person->ItemId);
+        $item = Item::find($event->itemId);
 
         if ($item->CompletionStatusId === 1) {
             $item->CompletionStatusId = 2;
 
             // because of the current MySQL trigger we also need to set the
             // TranscriptionStatusId otherwise it will reset the CompletionStatusId
-            // can/should be removed when removing the MySQL trigger
+            // can/should be removed when removing or changing the MySQL trigger
             $item->TranscriptionStatusId = 2;
-
-            $item->save();
         }
+
+        if ($item->TaggingStatusId === 1) {
+            $item->TaggingStatusId = 2;
+        }
+
+        $item->save();
     }
 }
