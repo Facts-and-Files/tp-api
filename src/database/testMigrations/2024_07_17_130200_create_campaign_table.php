5e8fe9e7de66e3e2497d39911f3bc144cba4b4f0
trenc <github@tommyschmucker.de>
2024-07-17 13:23:55 +0200

    tests: add more migrations

---
+++ src/database/testMigrations/2024_07_17_130200_create_campaign_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('Campaign', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+
+            $table->increments('CampaignId');
+        });
+
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('Campaign');
+    }
+};
+
