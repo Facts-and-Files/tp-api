d5638a899e1119f2992bb57aaa23f1d22cea4255
trenc <github@tommyschmucker.de>
2024-07-30 12:19:09 +0200

    tests: add tests and migration for items in import

--- src/database/testMigrations/2024_06_13_123800_create_item_table.php
+++ src/database/testMigrations/2024_06_13_123800_create_item_table.php
@@ -18,7 +18,14 @@ return new class extends Migration
             $table->smallInteger('TranscriptionStatusId')->default(1);
             $table->smallInteger('TaggingStatusId')->default(1);
             $table->smallInteger('LocationStatusId')->default(1);
+            $table->string('Title', 1000)->nullable();
+            $table->text('Description')->nullable();
+            $table->text('ImageLink');
+            $table->integer('OrderIndex');
+            $table->text('Manifest')->nullable();
+            $table->text('edm:WebResource')->nullable();
             $table->dateTime('LastUpdated')->useCurrent();
+            $table->dateTime('Timestamp')->useCurrent();
         });
     }
 
--- src/tests/Feature/ImportTest.php
+++ src/tests/Feature/ImportTest.php
@@ -63,7 +63,24 @@ class ImportTest extends TestCase
                     'Agent' => ''
                 ]
             ],
-            'Items' => []
+            'Items' => [
+                [
+                    'Title' => 'Test Story 1 Item 1',
+                    'Description' => '',
+                    'ImageLink' => 'ImageLink 1',
+                    'OrderIndex' => 1,
+                    'Manifest' => '',
+                    'edm:WebResource' => ''
+                ],
+                [
+                    'Title' => 'Test Story 1 Item 2',
+                    'Description' => '',
+                    'ImageLink' => 'ImageLink 2',
+                    'OrderIndex' => 2,
+                    'Manifest' => '',
+                    'edm:WebResource' => ''
+                ]
+            ]
         ],
         [
             'Story' => [
@@ -119,7 +136,16 @@ class ImportTest extends TestCase
                     'Agent' => ''
                 ]
             ],
-            'Items' => []
+            'Items' => [
+                [
+                    'Title' => 'Test Story 2 Item 1',
+                    'Description' => '',
+                    'ImageLink' => 'Image Link',
+                    'OrderIndex' => 1,
+                    'Manifest' => '',
+                    'edm:WebResource' => ''
+                ]
+            ]
         ]
     ];
 
@@ -156,7 +182,7 @@ class ImportTest extends TestCase
             ->assertJson($awaitedData);
     }
 
-    public function testPartialSuccessfulImport(): void
+    public function testPartialSuccessfulStoryImport(): void
     {
         $partialImportData = self::$importData;
         $partialImportData[1]['Story']['Dc']['Title'] = null;
@@ -188,4 +214,41 @@ class ImportTest extends TestCase
             ->assertJson($awaitedData);
     }
 
+    public function testPartialSuccessfulItemImport(): void
+    {
+        $partialImportData = self::$importData;
+        $partialImportData[0]['Items'][1]['ImageLink'] = null;
+        $awaitedSuccess = ['success' => true];
+        $awaitedData = [
+            'data' => [
+                [
+                    'StoryId' => 1,
+                    'ExternalRecordId' => self::$importData[0]['Story']['ExternalRecordId'],
+                    'RecordId' => self::$importData[0]['Story']['RecordId'],
+                    'dc:title' => self::$importData[0]['Story']['Dc']['Title'],
+                ],
+                [
+                    'StoryId' => 2,
+                    'ExternalRecordId' => self::$importData[1]['Story']['ExternalRecordId'],
+                    'RecordId' => self::$importData[1]['Story']['RecordId'],
+                    'dc:title' => self::$importData[1]['Story']['Dc']['Title'],
+                ],
+            ],
+            'error' => [
+                [
+                    'ExternalRecordId' => $partialImportData[0]['Story']['ExternalRecordId'],
+                    'RecordId' => $partialImportData[0]['Story']['RecordId'],
+                    'ItemOrderIndex' => 2,
+                    'error'    => []
+                ]
+            ]
+        ];
+
+        $response = $this->post(self::$endpoint, $partialImportData);
+
+        $response
+            ->assertStatus(207)
+            ->assertJson($awaitedSuccess)
+            ->assertJson($awaitedData);
+    }
 }
