7bde41fcd7852ea33926af38b2e58a0e0ac2521a
trenc <github@tommyschmucker.de>
2025-07-10 11:12:27 +0200

    tests: enhance test seeder and migration with Description field

--- src/database/seeders/PropertyDataSeeder.php
+++ src/database/seeders/PropertyDataSeeder.php
@@ -9,14 +9,16 @@ class PropertyDataSeeder extends Seeder
 {
     public static $data = [
         [
-            'PropertyId'     => 1,
-            'Value'          => 'German',
-            'PropertyTypeId' => 1
+            'PropertyId' => 1,
+            'Value'  => 'German',
+            'Description' => 'Description for Test Propery 1',
+            'PropertyTypeId' => 1,
         ],
         [
-            'PropertyId'     => 2,
-            'Value'          => 'Handwritten',
-            'PropertyTypeId' => 2
+            'PropertyId' => 2,
+            'Value' => 'Handwritten',
+            'Description' => 'Description for Test Propery 2',
+            'PropertyTypeId' => 2,
         ],
     ];
 
