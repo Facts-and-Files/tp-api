ec1befda410d376fd4d74dee6720c581310d954b
trenc <github@tommyschmucker.de>
2025-07-21 15:40:47 +0200

    feat: enhance places endpoint with new filters, and use a database view

---
+++ src/database/migrations/2025_07_21_125300_create_place_details_view.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        DB::statement('
+            CREATE VIEW place_combined_details_view AS
+            SELECT
+                p.PlaceId,
+                p.ItemId,
+                p.Name,
+                p.Latitude,
+                p.Longitude,
+                p.WikidataId,
+                p.WikiDataName,
+                p.Link,
+                p.Comment,
+                p.UserGenerated,
+                p.UserId,
+                p.PlaceRole,
+                i.Title as ItemTitle,
+                i.StoryId,
+                `s`.`dc:title` as StoryTitle,
+                s.ProjectId
+            FROM Place p
+            LEFT JOIN Item i ON p.ItemId = i.ItemId
+            LEFT JOIN Story s ON i.StoryId = s.StoryId
+
+            UNION ALL
+
+            SELECT
+                NULL as PlaceId,
+                NULL as ItemId,
+                s.PlaceName as Name,
+                s.PlaceLatitude as Latitude,
+                s.PlaceLongitude as Longitude,
+                s.PlaceLink as WikidataId,
+                s.PlaceComment as WikiDataName,
+                NULL as Link,
+                NULL as Comment,
+                s.PlaceUserGenerated as UserGenerated,
+                s.PlaceUserId as UserId,
+                "StoryPlace" as PlaceRole,
+                NULL as ItemTitle,
+                s.StoryId,
+                `s`.`dc:title` as StoryTitle,
+                s.ProjectId
+            FROM Story s
+            WHERE s.PlaceName IS NOT NULL
+        ');
+    }
+
+    public function down(): void
+    {
+        DB::statement('DROP VIEW IF EXISTS place_combined_details_view');
+    }
+};
