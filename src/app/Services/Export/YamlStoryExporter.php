345dbd09807c915620fadbfaa96f29b9abebece9
trenc <github@tommyschmucker.de>
2026-02-04 14:51:43 +0100

    test: enhance ItemDataSeeder

---
+++ src/app/Services/Export/YamlStoryExporter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services\Export;
+
+use App\Models\Story;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\Yaml\Yaml;
+
+class YamlStoryExporter implements StoryExporterInterface
+{
+    public function __construct(
+        private ModelTransformer $modelTransformer,
+    ) {
+    }
+
+    public function export(Story $story): StreamedResponse
+    {
+        $storyId = $story['StoryId'] ?? 'unknown';
+        $filename = "story-{$storyId}-" . now()->format('Ymd-His') . '.yml';
+
+        return response()->streamDownload(
+            callback: function () use ($story) {
+                echo $this->formatStoryAsYaml($story);
+            },
+            name: $filename,
+            headers: [
+                'Content-Type' => 'application/yaml; charset=utf-8',
+                'Content-Disposition' => 'attachment; filename="'.$filename.'"',
+            ]
+        );
+    }
+
+    private function formatStoryAsYaml(Story $story): string
+    {
+        $data = [
+            ...$this->modelTransformer->transformStory($story, ['ItemIds']),
+            ...$this->modelTransformer->addItemData($story->ItemIds),
+        ];
+        $yaml = Yaml::dump(
+            $data, 4, 2,
+            Yaml::DUMP_MULTI_LINE_LITERAL_BLOCK,
+            // enable with newer version ^7.4
+            /* Yaml::DUMP_NULL_AS_EMPTY, */
+            /* Yaml::DUMP_COMPACT_NESTED_MAPPING, */
+        );
+
+        return $yaml;
+    }
+}
