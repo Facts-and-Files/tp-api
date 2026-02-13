8379eab8623f0164d8a672eac1fd1e22d7b15d02
trenc <github@tommyschmucker.de>
2026-02-13 14:25:36 +0100

    test: test for properties on csv

--- src/tests/Feature/StoryExportTest.php
+++ src/tests/Feature/StoryExportTest.php
@@ -96,7 +96,7 @@ class StoryExportTest extends TestCase
         $content = $response->streamedContent();
         $parsedYaml = Yaml::parse($content);
 
-        $this->assertEquals('German', $parsedYaml['Items'][0]['Description']['Language']);
+        $this->assertEquals('German', $parsedYaml['Items'][0]['Description']['Language'][0]);
         $this->assertEquals(ItemDataSeeder::$data[2]['Description'], $parsedYaml['Items'][0]['Description']['Text']);
     }
 
@@ -109,10 +109,8 @@ class StoryExportTest extends TestCase
         $content = $response->streamedContent();
         $parsedYaml = Yaml::parse($content);
 
-        $this->assertEquals(
-            'German, English',
-            $parsedYaml['Items'][0]['Transcription']['Language'],
-        );
+        $this->assertEquals('German', $parsedYaml['Items'][0]['Transcription']['Language'][0]);
+        $this->assertEquals('English', $parsedYaml['Items'][0]['Transcription']['Language'][1]);
     }
 
     public function test_export_story_to_yaml_returns_correct_properties_data(): void
@@ -130,16 +128,21 @@ class StoryExportTest extends TestCase
         $this->assertEquals('Description for Test Property 1', $property['Description']);
     }
 
-    public function test_export_story_to_csv_returns_zip_file(): void
+    public function test_export_story_to_csv_returns_zip_with_csv_files(): void
     {
-        $response = $this->get('/stories/1/items/export/csv');
+        $storyId = 1;
+        $response = $this->get("/stories/{$storyId}/items/export/csv");
 
         $zipContent = $this->getZipContentFromResponse($response);
         $zip = new ZipArchive();
         $tempZip = $this->createTempZipFile($zipContent);
 
         $this->assertTrue($zip->open($tempZip));
-        $this->assertEquals(2, $zip->numFiles);
+        $this->assertEquals(3, $zip->numFiles);
+
+        $this->assertStringContainsString("story-{$storyId}-", $zip->getNameIndex(0));
+        $this->assertStringContainsString("story-items-{$storyId}-", $zip->getNameIndex(1));
+        $this->assertStringContainsString("story-properties-{$storyId}-", $zip->getNameIndex(2));
 
         $zip->close();
         unlink($tempZip);
@@ -183,12 +186,32 @@ class StoryExportTest extends TestCase
         $this->assertCount(2, $records);
         $this->assertEquals(3, $records[1]['ItemId']);
         $this->assertEquals(5, $records[2]['ItemId']);
+    }
 
-        // what about properties CSV
-        // add property data extra and add it to items instead of getting it directly from model
-        // similar to modelTransformer->addItemData()
-        /* print_r($records); */
-        $this->assertEquals('German', $records[2]['Properties'[1]['Name']]);
+    public function test_export_story_with_no_properties_returns_empty_properties_csv(): void
+    {
+        $response = $this->get('/stories/2/items/export/csv');
+        $propertiesCsv = $this->extractFileFromZip($response, 2);
+
+        $this->assertLessThanOrEqual(100, strlen($propertiesCsv));
+    }
+
+    public function test_export_story_to_csv_contains_multiple_properties_for_same_item(): void
+    {
+        $response = $this->get('/stories/1/items/export/csv');
+        $propertiesCsv = $this->extractFileFromZip($response, 2);
+        $records = $this->parseCsv($propertiesCsv);
+
+        $itemProperties = array_filter($records, fn($record) => $record['ItemId'] == 1);
+
+        $this->assertGreaterThan(0, count($itemProperties));
+
+        foreach ($itemProperties as $property) {
+            $this->assertArrayHasKey('ItemId', $property);
+            $this->assertArrayHasKey('Type', $property);
+            $this->assertArrayHasKey('Name', $property);
+            $this->assertArrayHasKey('Description', $property);
+        }
     }
 
     private function getZipContentFromResponse($response): string
