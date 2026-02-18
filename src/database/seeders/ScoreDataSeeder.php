987d29ab241bcfd2ad1e81d8370a63788f06bd1b
trenc <github@tommyschmucker.de>
2026-02-18 11:39:11 +0100

    test: disable FK constraints checks for Score test migration

--- src/database/seeders/ScoreDataSeeder.php
+++ src/database/seeders/ScoreDataSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class ScoreDataSeeder extends Seeder
 {
@@ -52,6 +53,8 @@ class ScoreDataSeeder extends Seeder
 
     public function run(): void
     {
+        Schema::disableForeignKeyConstraints();
         DB::table('Score')->insert(self::$data);
+        Schema::enableForeignKeyConstraints();
     }
 }
