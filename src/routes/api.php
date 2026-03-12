595016de2364f7e5773394546deef270aa5f376c
trenc <github@tommyschmucker.de>
2026-03-12 15:35:34 +0100

    refactor: change method name, introduce service

--- src/routes/api.php
+++ src/routes/api.php
@@ -168,7 +168,7 @@ Route::middleware(['auth:api'])->group(function() {
     Route::post('/transcriptions', [TranscriptionController::class, 'store']);
     Route::get('/transcriptions/{id}', [TranscriptionController::class, 'show']);
 
-    Route::post('/import', [ImportController::class, 'store']);
+    Route::post('/import', [ImportController::class, 'import']);
 
     Route::post('/update-solr', [SolrController::class, 'update']);
     Route::get('/update-solr', [SolrController::class, 'update']);
