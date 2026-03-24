9f2fc7ba3a32d79a0d6399567ecab75df22ed918
trenc <github@tommyschmucker.de>
2026-03-24 13:33:43 +0100

    feat: add ProjectName to StoryExport model

--- src/app/Http/Controllers/StoryItemExportController.php
+++ src/app/Http/Controllers/StoryItemExportController.php
@@ -16,7 +16,7 @@ class StoryItemExportController extends ResponseController
 
     public function export(int $id, string $format): StreamedResponse|JsonResponse
     {
-        $story = Story::findOrFail($id);
+        $story = Story::findOrFail($id)->makeHidden('project')->append('ProjectName');
 
         $config = config("exports.formats.{$format}");
 
