7b8cf892c673ef03c72f8d0a43de84c2d891e907
trenc <github@tommyschmucker.de>
2026-08-12 13:02:33 +0200

    test: add basic tests for story endpoint

---
+++ src/tests/Feature/StoryTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Facades\Artisan;
+use Database\Seeders\StoryDataSeeder;
+use Database\Seeders\ProjectDataSeeder;
+use Tests\TestCase;
+
+class StoryTest extends TestCase
+{
+    private static $endpoint = 'stories';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        self::populateTable();
+    }
+
+    public static function populateTable(): void
+    {
+        Artisan::call('db:seed', ['--class' => StoryDataSeeder::class]);
+        Artisan::call('db:seed', ['--class' => ProjectDataSeeder::class]);
+    }
+
+    public function test_get_all_stories(): void
+    {
+        $awaitedSuccess = ['success' => true];
+
+        $response = $this->get(self::$endpoint);
+
+        $response
+            ->assertOk()
+            ->assertJson($awaitedSuccess)
+            ->assertJsonCount(3, 'data');
+    }
+
+    public function test_get_a_non_existent_story_returns_404(): void
+    {
+        $queryParams = '/999999999999';
+        $awaitedSuccess = ['success' => false];
+
+        $response = $this->get(self::$endpoint . $queryParams);
+
+        $response
+            ->assertNotFound()
+            ->assertJson($awaitedSuccess);
+    }
+
+    public function test_get_basic_data_of_a_single_story(): void
+    {
+        $queryParams = '/' . StoryDataSeeder::$data[0]['StoryId'];
+        $awaitedSuccess = ['success' => true];
+        $awaitedData = StoryDataSeeder::$data[0];
+
+        $response = $this->get(self::$endpoint . $queryParams);
+
+        $response
+            ->assertOk()
+            ->assertJson($awaitedSuccess)
+            ->assertJsonFragment(['StoryId' => $awaitedData['StoryId']]);
+    }
+
+    public function test_update_data_of_a_single_story(): void
+    {
+        $queryParams = '/' . StoryDataSeeder::$data[0]['StoryId'];
+        $awaitedSuccess = ['success' => true];
+        $updateData = [
+            'Dc' => ['Title' => 'Updated-Test-Title'],
+            'Place' => [
+                'Name' => 'Updated-Name',
+                'WikiDataId' => 'Updated-WikidataId',
+            ],
+        ];
+
+        $response = $this->put(self::$endpoint . $queryParams, $updateData);
+
+        $response
+            ->assertOk()
+            ->assertJson($awaitedSuccess)
+            ->assertJson(['data' => $updateData]);
+    }
+
+    public function test_delete_a_story(): void
+    {
+        $datasetId = StoryDataSeeder::$data[1]['StoryId'];
+        $queryParams = '/' . $datasetId;
+        $awaitedSuccess = ['success' => true];
+        $awaitedData = ['data' => StoryDataSeeder::$data[1]];
+
+        $response = $this->delete(self::$endpoint . $queryParams);
+
+        $response
+            ->assertOk()
+            ->assertJson($awaitedSuccess);
+    }
+}
