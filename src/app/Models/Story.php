0029cbd879c179abb7238bfde20c40409aa168dd
trenc <github@tommyschmucker.de>
2026-08-12 13:03:46 +0200

    fix: remove unues import

--- src/app/Models/Story.php
+++ src/app/Models/Story.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Collection;
-use PhpParser\Node\Expr\FuncCall;
 
 class Story extends Model
 {
